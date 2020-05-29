49873f9a9c807545bb60b2127f48f7644ef62cb4
Luther Karper <luther.karper@live.com>
2020-05-29 13:10:37 -0400

    Add skeleton and homepage

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <>
+                <h2>
+                    Welcome to 'Find Something to Learn'!
+                </h2>
+                <p>
+                    FStL is a trivia app the helps you find what you don't know!  
+                    By attempting to answer trvia questions, you can figure out what you need to learn more about!
+                    There are two ways to play: Pub Triva Style and Jeopardy! Style.
+                    Pub Trivia Style allows you to answer multiple-choice or True/False questions (or a mix of the two)
+                    in a category of your choice or a random mix of all the categories!
+                    Jeopardy! Style allows you to answer questions that have appeared on real episodes of the hit television
+                    show "Jeopardy!".  This mode is more challenging because there are no options to pick from when answering a question! 
+                </p>
+                <p>
+                    Whichever style you choose, while answering questions, you will have the option to click a button to instantly search
+                    Wikipedia for information related to the question that you've just attempted to answer!  In this way, you can easily...
+                    <b>Find Something to Learn!</b>
+                </p>
+            </>
+        );    
+    }
+}
+
+export default HomePage;
---
+++ src/Main/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HomePage from '../HomePage/HomePage';
+
+class Main extends Component {
+    render() {
+        return (
+            <main>
+                <HomePage />
+            </main>
+        );
+    }
+}
+
+export default Main;
