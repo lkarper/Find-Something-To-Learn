652c7092b9261a730fe8660cc6869d8ebb368a21
Luther Karper <luther.karper@live.com>
2020-06-02 16:43:46 -0400

    Add defaultProps and PropTypes to JeopardyQuestion

--- src/PubLearn/PubLearn.js
+++ src/PubLearn/PubLearn.js
@@ -1,33 +1,67 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import AppContext from '../AppContext/AppContext';
 
 class PubLearn extends Component {
+
+    static defaultProps = {
+        match: {
+            params: {
+                qId: '',
+            },
+        },
+        location: {
+            state: {
+                correct: false,
+            },
+        },
+    }
 
     static contextType = AppContext;
 
     render() {
 
-        const currentId = this.props.match.params.qId;
-        const currentQuestion = decodeURIComponent(this.context.questions[currentId].question);
-        const correctAnswer = decodeURIComponent(this.context.questions[currentId].correct_answer);
+        const errorHTML = (
+            <>
+                <h3>Error</h3>
+                <p>Sorry, looks like something went wrong and the question and answer could not load.</p>
+            </>
+        );
+
+        if (this.props.match.params.qId && Object.keys(this.context).includes('questions')) {
+            const currentId = this.props.match.params.qId;
+            const currentQuestion = decodeURIComponent(this.context.questions[currentId].question);
+            const correctAnswer = decodeURIComponent(this.context.questions[currentId].correct_answer);
+
+            return (
+                <div className="Learn__lContainer">
+                    {this.props.location.state.correct ? "Congrats! You got it right!" : "Sorry, that's not the right answer."}
+                    <h3>Question: </h3>
+                    <p>{currentQuestion}</p>
+                    <h3>Correct answer:</h3>
+                    <p>{correctAnswer}</p>
+                    <Link 
+                        to={parseInt(currentId) + 1 === this.context.totalQuestions ? `/pubstyle/${currentId}/final` : `/pubstyle/${(parseInt(currentId)+1)}`}
+                    >Next Question</Link>
+                    <Link 
+                        to={`/pubstyle/${currentId}/wiki`}
+                    >Learn Something with Wikipedia!</Link>                    
+                </div>
+            );
+        }
 
         return (
             <div className="Learn__lContainer">
-                {this.props.location.state.score ? "Congrats! You got it right!" : "Sorry, that's not the right answer."}
-                <h3>Question: </h3>
-                <p>{currentQuestion}</p>
-                <h3>Correct answer:</h3>
-                <p>{correctAnswer}</p>
-                <Link 
-                    to={parseInt(currentId) + 1 === this.context.totalQuestions ? `/pubstyle/${currentId}/final` : `/pubstyle/${(parseInt(currentId)+1)}`}
-                >Next Question</Link>
-                <Link 
-                    to={`/pubstyle/${currentId}/wiki`}
-                >Learn Something with Wikipedia!</Link>                    
+                {errorHTML}
             </div>
         );
     }
+}
+
+PubLearn.propTypes = {
+    match: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
 }
 
 export default PubLearn;
