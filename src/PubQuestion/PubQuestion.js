cfb753e6d635eea1e9936fd97b1cf9c6f998141b
Luther Karper <luther.karper@live.com>
2020-06-02 17:06:35 -0400

    Update PubQuestion component
    Updated PubQuestion component so that, when context is not provided,
    the component renders and displays an error message.

--- src/PubQuestion/PubQuestion.js
+++ src/PubQuestion/PubQuestion.js
@@ -34,64 +34,83 @@ class PubQuestion extends Component {
     }
 
     componentDidMount() {
-        const currentQuestionIndex = this.props.match.params.qId;
-        const questionObject = this.context.questions[currentQuestionIndex];
-        if (questionObject.type === 'multiple') {
-            const { correct_answer, incorrect_answers } = questionObject;
-            const choices = [...incorrect_answers, correct_answer];
-            const shuffledChoices = [];
-            for (let i = 0; i < 4; i++) {
-                const randomIndex = Math.floor(Math.random() * choices.length);
-                shuffledChoices.push(choices[randomIndex]);
-                choices.splice(randomIndex, 1);
-            };
+        if (this.props.match.params.qId && Object.keys(this.context).includes('questions')) {
+            const currentQuestionIndex = this.props.match.params.qId;
+            const questionObject = this.context.questions[currentQuestionIndex];
+            if (questionObject.type === 'multiple') {
+                const { correct_answer, incorrect_answers } = questionObject;
+                const choices = [...incorrect_answers, correct_answer];
+                const shuffledChoices = [];
+                for (let i = 0; i < 4; i++) {
+                    const randomIndex = Math.floor(Math.random() * choices.length);
+                    shuffledChoices.push(choices[randomIndex]);
+                    choices.splice(randomIndex, 1);
+                };
 
-            this.setState({
-                shuffledChoices,
-            });
+                this.setState({
+                    shuffledChoices,
+                });
+            }
         }
     }
 
-    render() { 
-        const { totalQuestions, correctAnswerqIds } = this.context;
-        const currentQuestionIndex = this.props.match.params.qId;
-        const { category, difficulty, question, type } = this.context.questions[currentQuestionIndex];
-        let choicesHTML;
-        if (type === 'multiple') {
-            choicesHTML = this.state.shuffledChoices.map((choice, i) => 
-                <div key={i}>
-                    <input id={i} type="radio" name="choice" value={choice} onChange={e => this.handleChoiceChange(e.target.value)}/>
-                    <label htmlFor={i}>{decodeURIComponent(choice)}</label>
-                </div>
+    render() {
+
+        const errorHTML = (
+            <>
+                <h3>Error</h3>
+                <p>Sorry, but it looks like something went wrong and the next question could not be loaded.</p>
+            </>
+        );
+
+        if (this.props.match.params.qId && Object.keys(this.context).includes('questions')) { 
+            const { totalQuestions, correctAnswerqIds } = this.context;
+            const currentQuestionIndex = this.props.match.params.qId;
+            const { category, difficulty, question, type } = this.context.questions[currentQuestionIndex];
+            let choicesHTML;
+            if (type === 'multiple') {
+                choicesHTML = this.state.shuffledChoices.map((choice, i) => 
+                    <div key={i}>
+                        <input id={i} type="radio" name="choice" value={choice} onChange={e => this.handleChoiceChange(e.target.value)}/>
+                        <label htmlFor={i}>{decodeURIComponent(choice)}</label>
+                    </div>
+                    );
+            } else {
+                choicesHTML = (
+                    <>
+                        <div>
+                            <input id="true" type="radio" name="choice" value="True" onChange={e => this.handleChoiceChange(e.target.value)}/>
+                            <label htmlFor="true">True</label>
+                        </div>
+                        <div>
+                            <input id="false" type="radio" name="choice" value="False" onChange={e => this.handleChoiceChange(e.target.value)}/>
+                            <label htmlFor="false">False</label>
+                        </div>
+                    </>
                 );
-        } else {
-            choicesHTML = (
-                <>
-                    <div>
-                        <input id="true" type="radio" name="choice" value="True" onChange={e => this.handleChoiceChange(e.target.value)}/>
-                        <label htmlFor="true">True</label>
-                    </div>
-                    <div>
-                        <input id="false" type="radio" name="choice" value="False" onChange={e => this.handleChoiceChange(e.target.value)}/>
-                        <label htmlFor="false">False</label>
-                    </div>
-                </>
+            }
+
+            return (
+                <div className="PubQuestion__qcontainer">
+                    <p>Question {parseInt(currentQuestionIndex) + 1} of {totalQuestions}</p>
+                    <p>Score: {correctAnswerqIds.length}/{totalQuestions}</p>
+                    <p>Category: {decodeURIComponent(category)}</p>
+                    <p>Type: {decodeURIComponent(type)}</p>
+                    <p>Difficulty: {decodeURIComponent(difficulty)}</p>
+                    <form onSubmit={event => this.handleSubmit(event)}>
+                        <fieldset>
+                            <legend>{decodeURIComponent(question)}</legend>
+                            {choicesHTML}
+                        </fieldset>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
             );
         }
+
         return (
             <div className="PubQuestion__qcontainer">
-                <p>Question {parseInt(currentQuestionIndex) + 1} of {totalQuestions}</p>
-                <p>Score: {correctAnswerqIds.length}/{totalQuestions}</p>
-                <p>Category: {decodeURIComponent(category)}</p>
-                <p>Type: {decodeURIComponent(type)}</p>
-                <p>Difficulty: {decodeURIComponent(difficulty)}</p>
-                <form onSubmit={event => this.handleSubmit(event)}>
-                    <fieldset>
-                        <legend>{decodeURIComponent(question)}</legend>
-                        {choicesHTML}
-                    </fieldset>
-                    <button type="submit">Submit</button>
-                </form>
+                {errorHTML}
             </div>
         );
     }
