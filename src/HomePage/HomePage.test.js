cdf8c87381e9fbc64871e261d9973ead07f468f4
Luther Karper <luther.karper@live.com>
2020-06-03 10:31:37 -0400

    Add snapshot test to HomePage component

--- src/HomePage/HomePage.test.js
+++ src/HomePage/HomePage.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import HomePage from './HomePage';
 
 describe('HomePage component', () => {
@@ -12,5 +14,10 @@ describe('HomePage component', () => {
             </BrowserRouter>, 
             div);
         ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const wrapper = shallow(<HomePage />);
+        expect(toJSON(wrapper)).toMatchSnapshot();
     });
 });
