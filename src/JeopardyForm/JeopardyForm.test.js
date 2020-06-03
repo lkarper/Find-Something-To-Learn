089047f045410f0de26964b5d31267ad08141289
Luther Karper <luther.karper@live.com>
2020-06-03 11:16:10 -0400

    Add snapshot test to JeopardyForm component

--- src/JeopardyForm/JeopardyForm.test.js
+++ src/JeopardyForm/JeopardyForm.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import JeopardyForm from './JeopardyForm';
 
 describe('JeopardyForm component', () => {
@@ -7,5 +9,10 @@ describe('JeopardyForm component', () => {
         const div = document.createElement('div');
         ReactDOM.render(<JeopardyForm />, div);
         ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const wrapper = shallow(<JeopardyForm />);
+        expect(toJSON(wrapper)).toMatchSnapshot();
     });
 });
